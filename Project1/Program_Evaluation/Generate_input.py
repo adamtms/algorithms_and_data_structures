1281b3fda6593a43742ed8635487af4d3dc148e5
adam <adamtms@hotmail.com>
2023-03-08 16:33:30 +0100

    Add get time.

--- Project1/Program_Evaluation/Generate_input.py
+++ Project1/Program_Evaluation/Generate_input.py
@@ -1,6 +1,6 @@
 from random import randint
 
-def generate_random(n = int(1e4), rang=None):
+def generate_random(n=1000, rang=None):
     if rang is None:
         rang = n
     result = []
@@ -8,17 +8,17 @@ def generate_random(n = int(1e4), rang=None):
         result.append(randint(0, rang+1))
     return result
 
-def generate_increasing(n = int(1e4), rang=None):
+def generate_increasing(n=1000, rang=None):
     result = generate_random(n, rang)
     result.sort()
     return result
 
-def generate_decreasing(n = int(1e4), rang=None):
+def generate_decreasing(n=1000, rang=None):
     result = generate_random(n, rang)
     result.sort(reverse=True)
     return result
 
-def generate_v_shape(n = int(1e4), rang=None):
+def generate_v_shape(n=1000, rang=None):
     half = n // 2
     first = generate_increasing(half, n)
     last = generate_decreasing(half, n)
