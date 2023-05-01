c949696da5adc3113f7291688e29879a4e1e17b6
DESKTOP-2KVU6PK\Marcin <marcin.kapiszewski@gmail.com>
2023-05-01 07:48:14 +0200

    Added graphs representation scheme

---
+++ Project3/graph_representations/digraphRepresentation.py
@@ -0,0 +1,16 @@
+class Digraph:
+    def __init__(self):
+        pass
+    def addNode(self, node):
+        pass
+    def addEdge(self, startNode, endNode):
+        pass
+    def checkEdge(self, startNode, endNode):
+        # return true if enge exists, false otherwise
+        pass
+    def getEdges(self):
+        # return list of all edges
+        pass
+    def getSuccesors(self, node):
+        # return list of all successors of given node
+        pass
