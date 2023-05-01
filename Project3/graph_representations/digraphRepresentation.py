9298adcd8b98f2dbe7c07695625f60e7aa91c5de
DESKTOP-2KVU6PK\Marcin <marcin.kapiszewski@gmail.com>
2023-05-01 08:54:42 +0200

    Added some tests to diGraphs

--- Project3/graph_representations/digraphRepresentation.py
+++ Project3/graph_representations/digraphRepresentation.py
@@ -14,3 +14,41 @@ class Digraph:
     def getSuccesors(self, node):
         # return list of all successors of given node
         pass
+
+if __name__ == "__main__":
+
+    def addNodes(graph, nodes):
+        for node in nodes:
+            graph.addNode(node)
+
+    def addEdges(graph, edges):
+        for startNode, endNode in edges:
+            graph.addEdge(startNode, endNode)
+
+    graph = Digraph()
+    nodes1 = [i for i in range(5)]
+    edges1 = [ (1, 2), (1, 3), (1, 4), (1, 0), (2, 4), (2, 3), (2, 0), (0, 3), (0, 4) ]
+    nodes2 = [i for i in range(5, 10)]
+    edges2 = [ (1, 9), (1, 6), (4, 5), (3, 7), (2, 4), (2, 3) ]
+    addNodes(graph, nodes1)
+    addEdges(graph, edges1)
+    print(graph.getEdges())
+    print(f"Should be {edges1}")
+    print(graph.getSuccesors(1))
+    print("Should be [2, 3, 4, 0]")
+    print(graph.getSuccesors(4))
+    print("Should be []")
+    print(f"{graph.checkEdge(4, 5)} == False")
+    print(f"{graph.checkEdge(1, 2)} == True")
+    print(f"{graph.checkEdge(0, 1)} == False")
+    addNodes(graph, nodes2)
+    addEdges(graph, edges2)
+    print(graph.getEdges())
+    print(f"Should be {edges1 + edges2}")
+    print(graph.getSuccesors(1))
+    print("Should be [2, 3, 4, 0, 9, 6]")
+    print(graph.getSuccesors(4))
+    print("Should be [5]")
+    print(f"{graph.checkEdge(4, 5)} == True")
+    print(f"{graph.checkEdge(1, 2)} == True")
+    print(f"{graph.checkEdge(0, 1)} == False")
