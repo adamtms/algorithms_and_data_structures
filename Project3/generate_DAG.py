9a86808b131e5de6e9e3a9fd94ebcdb10d62338d
adam <adamtms@hotmail.com>
2023-05-07 22:34:14 +0200

    Add times.

--- Project3/generate_DAG.py
+++ Project3/generate_DAG.py
@@ -1,11 +1,11 @@
 import random
 
-def generateDAG(nodesNum: int=100):
+def generateDAG(nodesNum: int=100, saturation: float = 1):
     initial_matrix = [[0] * (x + 1) + [1] * (nodesNum - x - 1) for x in range(nodesNum)]
     nodes = list(range(nodesNum))
     random.shuffle(nodes)
     matrix = [[0]*nodesNum for _ in range(nodesNum)]
     for startNodeIndex in range(nodesNum):
         for endNodeIndex in range(nodesNum):
-            matrix[startNodeIndex][endNodeIndex] = initial_matrix[nodes[startNodeIndex]][nodes[endNodeIndex]]
+            matrix[startNodeIndex][endNodeIndex] = 1 if (initial_matrix[nodes[startNodeIndex]][nodes[endNodeIndex]] and random.random() <= saturation) else 0
     return matrix
