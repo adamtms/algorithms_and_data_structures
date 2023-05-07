bd0064b6cd96e594c6a707916109f1b92bcc5519
adam <adamtms@hotmail.com>
2023-05-07 13:30:18 +0200

    Minor performance changes.

--- Project3/generate_DAG.py
+++ Project3/generate_DAG.py
@@ -1,15 +1,10 @@
 import random
 
-# Generate nested list, if elem in position x, y is equal to:
-#   -1, exists edge from y to x
-#    1, exists egde from x to y
-#    0, doesn't exists edge between x and y
-# Nodes are indexed from 0
 def generateDAG(nodesNum: int=100):
-    initial_matrix = [[0 for _ in range(x+1)] + [1 for _ in range(nodesNum - x - 1)] for x in range(nodesNum)]
-    nodes = [i for i in range(nodesNum)]
+    initial_matrix = [[0] * (x + 1) + [1] * (nodesNum - x - 1) for x in range(nodesNum)]
+    nodes = list(range(nodesNum))
     random.shuffle(nodes)
-    matrix = [[0 for _ in range(nodesNum)] for _ in range(nodesNum)]
+    matrix = [[0]*nodesNum for _ in range(nodesNum)]
     for startNodeIndex in range(nodesNum):
         for endNodeIndex in range(nodesNum):
             matrix[startNodeIndex][endNodeIndex] = initial_matrix[nodes[startNodeIndex]][nodes[endNodeIndex]]
