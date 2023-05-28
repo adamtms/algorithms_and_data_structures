39ace436abe49f913561631e1550af6cbb87d5d9
DESKTOP-2KVU6PK\Marcin <marcin.kapiszewski@gmail.com>
2023-05-28 11:34:09 +0200

    Added program to measure time of algorithm

---
+++ Project4/evaluateProgram.py
@@ -0,0 +1,7 @@
+import time
+
+def measureTime(function, functionInput: list):
+    start_time = time.time()
+    function(*functionInput)
+    program_time = (time.time() - start_time)
+    return program_time
