9540564217dd98a6bcbfc0e069f57d60456c9c54
adam <adamtms@hotmail.com>
2023-06-09 20:11:18 +0200

    Correct output of dynamic algorithm.

--- Project5/dynamic.py
+++ Project5/dynamic.py
@@ -3,7 +3,8 @@ def dynamic(instance: Instance) -> int:
     num_items = instance.num_items
     capacity = instance.capacity
     array = [[0]*(capacity+1)]
-    for weight, value in instance.items:
+    items = instance.items
+    for weight, value in items:
         row = []
         for size in range(capacity + 1):
             if weight > size: 
@@ -11,11 +12,25 @@ def dynamic(instance: Instance) -> int:
                 continue
             row.append(max(array[-1][size], array[-1][size - weight] + value))
         array.append(row)
-    return array[num_items][capacity], array
+
+    rem = capacity
+    res = array[num_items][capacity]
+    result = []
+    for i in range(num_items, 0, -1):
+        if res <= 0:
+            break
+        if res == array[i - 1][rem]:
+            continue
+        else:
+            weight, value = items[i - 1]
+            result.append((weight, value))
+            res = res - value
+            rem = rem - weight
+
+    return array[num_items][capacity], result
 
 if __name__ == "__main__":
     from generate import get_instance
     inst = get_instance()
     print(inst)
-    value, matrix = dynamic(inst)
-    print(value, *matrix, sep="\n")
+    print(dynamic(inst))
